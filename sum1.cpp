b33f010e8693b4864573216ed1e8aa13f5493b18
nixonnourie <128100689+nixonnourie@users.noreply.github.com>
2023-03-31 20:50:45 +1100

    Add files via upload

---
+++ sum1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1, num2, sum;
+
+  cout << "Enter the first integer: ";
+  cin >> num1;
+
+  cout << "Enter the second integer: ";
+  cin >> num2;
+
+  sum = num1 + num2;
+
+  cout << "The sum is: " << sum << endl;
+
+  return 0; // Add the missing curly brace here
+}
